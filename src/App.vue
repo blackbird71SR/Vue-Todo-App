6397a46d35733d98a7c2b5233dab6b08cc794303
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2020-07-24 12:03:03 +0530

    Add modal for adding todo

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,13 @@
 		<div class="todo-wrapper">
 			<div class="todo-container">
 				<TodoList :todos="todos" />
+				<div class="todo-create-btn-container">
+					<div class="app-button">Create</div>
+				</div>
 			</div>
+		</div>
+		<div class="modal">
+			<div class="modal-content">Modal Window</div>
 		</div>
 	</div>
 </template>
@@ -61,5 +67,31 @@ export default {
 		background-color: #ededed;
 		border-radius: 5px;
 	}
+}
+.modal {
+	display: none;
+	position: fixed;
+	z-index: 9999;
+	left: 0;
+	top: 0;
+	width: 100%;
+	height: 100%;
+	overflow: auto; //enable scroll if needed
+	background-color: rgba(0, 0, 0, 0.4);
+}
+.modal-content {
+	background-color: #fefefe;
+	margin: 15% auto;
+	padding: 20px;
+	border: 1px solid #888;
+	width: 80%;
+}
+.app-button {
+	font-size: 20px;
+	padding: 10px;
+	border-radius: 5px;
+	background-color: #795899;
+	color: white;
+	font-weight: bold;
 }
 </style>
